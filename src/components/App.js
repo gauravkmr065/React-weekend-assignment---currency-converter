0c20b4055541cbb969386dd54188fae4f9af003b
gauravkmr065 <gs9814556@gmail.com>
2021-05-18 16:43:18 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,72 @@
-import React  from "react";
+import React, {useState,useEffect}  from "react";
 import CurrencyConverter from './CurrencyConverter';
 import '../styles/App.css';
+const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=65d067ce7b1b523af7ace5fa7d0f1d6e';
 
+ 
 const App = () => {
+   const[currencyOption,setCurrencyOption]= useState([]);
+    const[fromCurr,setFromcurr] = useState();
+    const[toCurr,setTocurr] = useState();
+    const [exchangeRate, setExchangeRate]= useState();
+    const[amount, setAmount] =useState(1);
+    const[amountFromCurr,setAmountFromcurr]= useState(true);
+   
+    let toAmount , fromAmount ;
+    if(amountFromCurr){
+      fromAmount=amount;
+      toAmount=amount*exchangeRate;
+    }else{
+      toAmount= amount;
+      fromAmount =amount/exchangeRate;
+    }
+
+  useEffect(()=>{
+    fetch(BASE_URL)
+     .then(res=>res.json())
+     .then(data=>{
+       const Firstcurr = Object.keys(data.rates)[0];
+       setCurrencyOption([data.base, ...Object.keys(data.rates)])
+       setFromcurr(data.base);
+       setTocurr(Firstcurr);
+       setExchangeRate(data.rates[Firstcurr]);
+     });
+ },[])
+
+ useEffect(()=>{
+     if(fromCurr!=null && toCurr!=null){
+         fetch(`{BASE_URL}?base=${fromCurr}&symbols=${toCurr}`)
+         .then(res=>res.json())
+         .then(data=>setExchangeRate(data.rates[toCurr]));
+     }
+ },[fromCurr,toCurr])
+
+ function handleFromAmount(e){
+   setAmount(e.target.value);
+   setAmountFromcurr(true);
+ }
+ function handleToAmount(e){
+   setAmount(e.target.value);
+   setAmountFromcurr(false);
+ }
+ 
   return (
     <div id="main">
-      <CurrencyConverter/>
+      <h1>Currency Converter</h1>
+      <br/>
+      <CurrencyConverter 
+            currencyOption={currencyOption} 
+            selectedCurr={fromCurr}
+            onchangeCurr={e=>setFromcurr(e.target.value)}
+            amount = {fromAmount}
+            onchangeAmount ={handleFromAmount}
+            />
+      <br/>
+      <CurrencyConverter  currencyOption={currencyOption} selectedCurr={toCurr}
+          onchangeCurr={e=>setTocurr(e.target.value)}
+           amount = {toAmount}
+           onchangeAmount ={handleToAmount}
+       />
     </div>
   )
 }
--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../styles/App.css';
 
-export default function CurrencyConverter() {
+export default function CurrencyConverter(props) {
+    const {currencyOption,selectedCurr,onchangeCurr,amount,onchangeAmount} = props;
+    let i=1;
     return (
-        <div>
-            
+        <div id="curencyCon">
+            <select id="option" value={selectedCurr} onChange={onchangeCurr}>
+                {currencyOption.map(option => (
+                    <option key ={i++}value={option}>{option}</option>
+                ))}
+            </select> 
+            <input type="number" value={amount}  onChange={onchangeAmount}/>
         </div>
     )
 }
