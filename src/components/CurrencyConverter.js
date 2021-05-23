09eb15b666dd79e7ca7af4a4eb892c90f319c6cc
gaurav saxena <42857179+gauravkmr065@users.noreply.github.com>
2021-05-23 12:10:35 +0530

    Update CurrencyConverter.js

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -2,12 +2,11 @@ import React from 'react'
 import '../styles/App.css';
 
 export default function CurrencyConverter(props) {
-    const {currencyOption,selectedCurr,onchangeCurr,amount,onchangeAmount,defaultCurr} = props;
+    const {currencyOption,selectedCurr,onchangeCurr,amount,onchangeAmount} = props;
     let i=1;
     return (
         <div id="curencyCon">
             <select id="option" value={selectedCurr} onChange={onchangeCurr}>
-                <option>{defaultCurr}</option>
                 {currencyOption.map(option => {
                   return   <option key ={i++}value={option}>{option}</option>
               })}
